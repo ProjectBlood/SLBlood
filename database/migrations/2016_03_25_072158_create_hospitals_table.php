d921785e5e0b592dc6a05a51b4ece1d6adfb776b
Kasun Edward <kasunedward48@gmail.com>
2016-04-06 21:43:40 +0530

    Added files via upload

---
+++ database/migrations/2016_03_25_072158_create_hospitals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHospitalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hospitals', function (Blueprint $table) {
+            $table->increments('hospital_id');
+			$table->string('name');
+			$table->double('longitude');
+			$table->double('latitude');
+			$table->string('address');
+			$table->string('contact_no');
+			$table->string('key');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('hospitals');
+    }
+}
---
+++ database/migrations/2016_03_29_165315_create_donations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->increments('donation_id');
+			$table->string('request_id');
+			$table->string('donor_id');
+			$table->string('authorized_id');
+			$table->double('amount');
+			$table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('donations');
+    }
+}
